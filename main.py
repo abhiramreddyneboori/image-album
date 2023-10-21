d44bf21b765337b37fce6ea9dff9dda081689fea
abhiramreddyneboori <148569822+abhiramreddyneboori@users.noreply.github.com>
2023-10-21 03:53:48 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,22 +1,9 @@
-import os
-import logging
-from flask import Flask, request, jsonify, redirect
-from google.cloud import storage, firestore
-
+from flask import Flask
 app = Flask(__name__)
 
-# Initialize logging
-logging.basicConfig(level=logging.DEBUG)
-
-# Initialize Firestore
-db = firestore.Client()
-
-# Initialize Cloud Storage
-storage_client = storage.Client()
-bucket_name = 'image-upload-bucket4'
-bucket = storage_client.bucket(bucket_name)
-
-# Rest of your existing code...
+@app.route('/')
+def hello():
+    return "Hello World!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
